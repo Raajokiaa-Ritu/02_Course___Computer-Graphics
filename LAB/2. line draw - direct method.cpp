364b0c5f37c09f79be2cf84f7217edc61d57013b
Khandaker Jannatul Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2024-05-08 10:12:08 +0600

    Update 2. line draw - direct method.cpp

--- LAB/2. line draw - direct method.cpp
+++ LAB/2. line draw - direct method.cpp
@@ -6,14 +6,36 @@ using namespace std;
 int main()
 {
     int gd=DETECT,gm;
-    int x_initial,y_initial,x_final,y_final;
     initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-    printf("\n Please enter an initial coordinate of the line = ");
-    scanf("%d %d", &x_initial,&y_initial);
-    printf("\n Now, \n enter final coordinate of the line = ");
-    scanf("%d %d",&x_final,&y_final);
-    printf("\n***** LINE ******");
-    line(x_initial,y_initial,x_final,y_final);
+
+    int x1, y1, x2, y2, x, y, steps;
+    cout<<"enter x1 and y1 : ";
+    cin>>x1>>y1;
+    cout<<"enter x2 and y2 : ";
+    cin>>x2>>y2;
+
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float m = dy / dx ;
+    float b = x1 - m*y1;
+
+    if(dx < 0){
+        x = x2;
+        y = y2;
+        steps = x1;
+    }
+    else{
+        ///if dx>0
+        x = x1;
+        y = y1;
+        steps = x2;
+    }
+
+    while(x <= steps){
+        putpixel(x, y, GREEN);
+        x = x + 1;
+        y = m*x + b;
+    }
 
     getch();
     closegraph();
