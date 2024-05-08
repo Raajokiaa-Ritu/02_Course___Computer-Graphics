91a2ee1d1e0bb6a4e94a5e45958ae8c1b3d099ad
Khandaker Jannatul Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2024-05-08 11:14:36 +0600

    Update 4. line draw - Bresenham method.cpp

--- LAB/4. line draw - Bresenham method.cpp
+++ LAB/4. line draw - Bresenham method.cpp
@@ -1,50 +1,72 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
 #include<conio.h>
 #include<graphics.h>
+using namespace std;
+
 int main()
 {
       int gd = DETECT, gm;
-      int dx, dy, p, end;
-      float x1, x2, y1, y2, x, y;
       initgraph(&gd, &gm, "c:\tc\bgi");
-      printf("Enter Value of X1: ");
-      scanf("%f", &x1);
-      printf("Enter Value of Y1: ");
-      scanf("%f", &y1);
-      printf("Enter Value of X2: ");
-      scanf("%f", &x2);
-      printf("Enter Value of Y2: ");
-      scanf("%f", &y2);
-      dx = abs(x1 - x2);
-      dy = abs(y1 - y2);
-      p = 2 * dy - dx;
-      if(x1 > x2)
-      {
-            x = x2;
-            y = y2;
-            end = x1;
+      int x1,y1, x2,y2,x,y,p, steps;
+
+      printf("Enter x1 and y1: "); scanf("%d %d", &x1, &y1);
+      printf("Enter x2 and y2: "); scanf("%d %d", &x2, &y2);
+
+     ///konta small? x1 or x2?
+      if((x2-x1) < 0){
+        x = x2; y = y2;
       }
-      else
-      {
-            x = x1;
-            y = y1;
-            end = x2;
+      else{ ///if dx>0
+        x = x1; y = y1;
       }
-      putpixel(x, y, 10);
-      while(x < end)
-      {
-            x = x + 1;
-            if(p < 0)
-            {
-                  p = p + 2 * dy;
+
+      float dx = abs(x2- x1);
+      float dy = abs(y2- y1);
+
+      float m = dy / dx;
+
+
+      cout<<"start: "<<x <<" "<<y<<endl;
+      if(m>=1){
+          p = (2*dx) - dy;
+          steps = abs(y2-y1);
+          int i = 1;
+          while(i <= steps)
+          {
+              putpixel(x, y, WHITE);
+              y = y+1;
+              if(p < 0){
+                p = p + (2*dx);
+              }
+              else{
+                x = x+1;
+                p = p + 2*dx - 2*dy;
+              }
+              i++;
+          }
+      }
+
+      else{
+          p = (2*dy) - dx;
+          steps = abs(x2- x1);
+          //cout<<"last: "<<steps<<endl;
+          int i = 1;
+          while(i <= steps)
+          {
+            putpixel(x, y, WHITE);
+            x = x + 1;//cout<<"x = "<<x<<endl;
+            if(p<0){
+                p = p + 2*dy;
             }
-            else
-            {
-                  y = y + 1;
-                  p = p + 2 * (dy - dx);
+            else{
+                y = y+1;
+                p = p + (2*dy - 2*dx);
             }
-            putpixel(x, y, 10);
+            i++;
+          }
       }
+
+
       getch();
       closegraph();
 }
